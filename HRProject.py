a7ed225bc8e25dc41e50ec39daa6e6fc5b312198
mohsin-aslam <33390180+mohsin-aslam@users.noreply.github.com>
2017-12-06 23:29:50 +0500

    Update HRProject.py
    New combined features added and not useful features dropped

--- HRProject.py
+++ HRProject.py
@@ -21,3 +21,19 @@ def MergeDataSetToCsv(path,df):
     import pandas as pd
     df.to_csv(path,sep = ',',encoding='utf-8')
     return 
+df1 = LoadData('F:\\HRProject\\HR_IBM.csv')
+df2 = LoadData('F:\\HRProject\\HR_comma_sep.csv')
+result = df1.append(df2, ignore_index=True)
+
+import numpy as np
+result['satisfaction_level_Combined'] = np.where(result['satisfaction_level'].between(0.76,1), 4, result['satisfaction_level'])
+result['satisfaction_level_Combined'] = np.where(result['satisfaction_level_Combined'].between(0.51,0.75), 3, result['satisfaction_level_Combined'])
+result['satisfaction_level_Combined'] = np.where(result['satisfaction_level_Combined'].between(0.26,0.50), 2, result['satisfaction_level_Combined'])
+result['satisfaction_level_Combined'] = np.where(result['satisfaction_level_Combined'].between(0.0,0.25), 1, result['satisfaction_level_Combined'])   
+
+result.satisfaction_level_Combined.fillna(result.JobSatisfaction, inplace=True)
+
+result['YearsAtCompanyCombied'] = result.time_spend_company.fillna(result.YearsAtCompany)
+result_Combined = result.drop(['Over18','EmployeeCount','satisfaction_level','JobSatisfaction','time_spend_company','YearsAtCompany'],axis=1)
+    
+#result = result.drop('satisfaction_level_Combined',axis=1)
