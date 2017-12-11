fd9159281eb5c5fcc0e69bfb720a1c598773b24e
Wajih-Sid <wajihsid@gmail.com>
2017-12-11 16:52:47 +0500

    Change project structure

--- apiserver/apiserver/views.py
+++ apiserver/apiserver/views.py
@@ -3,10 +3,22 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
+from HRProject import PersistentModel
+
+
 @csrf_exempt
 def get_churn(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        return JsonResponse({'status': 'OK', 'data': data})
+
+        persistent_model = PersistentModel()
+
+        try:
+            predictions = persistent_model.predict_from_persistedmodel(data)
+            return JsonResponse({'status': 'OK', 'predictions': predictions, 'data': data})
+
+        except Exception:
+            print "Failed!"
+
     else:
         return JsonResponse({'status': 'OK'})
